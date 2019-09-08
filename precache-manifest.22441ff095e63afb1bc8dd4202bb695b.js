f094d4b936497ef7574153c4ad5ad0b3cba1c9ea
rwoelfle <rwoelfle@gmail.com>
2019-09-08 11:18:20 +0200

    Initial commit

---
+++ precache-manifest.22441ff095e63afb1bc8dd4202bb695b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb5faf60e99c95047c89b904cba1f30",
+    "url": "/Star_Wars_Plot_Generator/index.html"
+  },
+  {
+    "revision": "14923890fa672ae82f95",
+    "url": "/Star_Wars_Plot_Generator/static/css/main.26b60e01.chunk.css"
+  },
+  {
+    "revision": "08ce89a3ec7e3ae71dd5",
+    "url": "/Star_Wars_Plot_Generator/static/js/2.bfedd293.chunk.js"
+  },
+  {
+    "revision": "14923890fa672ae82f95",
+    "url": "/Star_Wars_Plot_Generator/static/js/main.0953d1fb.chunk.js"
+  },
+  {
+    "revision": "fdab13a1fe243d687c80",
+    "url": "/Star_Wars_Plot_Generator/static/js/runtime~main.2c956f7b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Star_Wars_Plot_Generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/Generator.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Generator from './Generator';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Generator />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
